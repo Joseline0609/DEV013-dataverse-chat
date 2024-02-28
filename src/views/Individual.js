ca436ba30332684d54c20e1e285e4369b0f10553
Etelbina <etelbinacanedo@gmail.com>
2024-02-28 02:07:06 -0600

    individual chat styles

--- src/views/Individual.js
+++ src/views/Individual.js
@@ -4,57 +4,61 @@ import { HomeIconButton } from "../components/HomeIconButton.js";
 
 export default function IndividualChat() {
   const viewIndividualChat = document.createElement("div");
-  viewIndividualChat.className = "chat-container";
+  viewIndividualChat.className = "chat-wrapper";
 
   viewIndividualChat.innerHTML += `
     <div id="chat-area" class="chat-area">
       <div id="info-area" class="info-area">
-        <img src="https://github.com/Etelbina/dataverse/blob/main/src/resources/Icons/Ornamentales.png?raw=true">
-        <h1>Plant Name</h1>
-        <p>Short description Lorem Ipsum es<br/>
-          simplemente el texto de relleno de<br/>
-          las imprentas y archivos de texto.
-        </p>
+        <div>
+          <img src="https://github.com/Etelbina/dataverse/blob/main/src/resources/Icons/Ornamentales.png?raw=true">
+        </div>
+        <div class="text-area">
+          <h1>Plant Name</h1>
+          <p>Short description Lorem Ipsum es
+            simplemente el texto de relleno de
+            las imprentas y archivos de texto.
+          </p>
+        </div>
       </div>
-      <div id="chat-container">
+      <div id="chat-container" class="chat-container">
         <div class="plant-message">
-          <img src="https://github.com/Etelbina/dataverse/blob/main/src/resources/Icons/Ornamentales.png?raw=true"
-            alt="Avatar" style="height:20px;width:20px";>
           <p>Hi 'user name'. I'm 'plant name'<br/>
             Would you like to ask me something?
           </p>
+          <img src="https://github.com/Etelbina/dataverse/blob/main/src/resources/Icons/Ornamentales.png?raw=true"
+            alt="Avatar" style="height:20px;width:20px";>
         </div>
         <div class="user-message">
           <p>Hi plant, I want to know how much water you need</p>
         </div>
       </div>
-        <textarea placeholder="..." id="user-text" required></textarea>
-      </div>
+        <textarea placeholder="..." id="user-text" class="user-text" required></textarea>
     </div>
   `;
   viewIndividualChat.append(HomeIconButton(), GroupIconButton());
 
-  const groupButton = document.createElement("button");
-  viewIndividualChat.appendChild(groupButton);
-  const textButton = document.createTextNode("Go group chat");
-  groupButton.appendChild(textButton);
+  // const groupButton = document.createElement("button");
+  // viewIndividualChat.appendChild(groupButton);
+  // const textButton = document.createTextNode("Go group chat");
+  // groupButton.appendChild(textButton);
 
-  groupButton.addEventListener("click", () => {
-    navigateTo("/Group", props);
-  });
+  // groupButton.addEventListener("click", () => {
+  //   navigateTo("/Group", props);
+  // });
 
-  const homeButton = document.createElement("button");
-  viewIndividualChat.appendChild(homeButton);
-  const text = document.createTextNode("Go home");
-  homeButton.appendChild(text);
+  // const homeButton = document.createElement("button");
+  // viewIndividualChat.appendChild(homeButton);
+  // const text = document.createTextNode("Go home");
+  // homeButton.appendChild(text);
 
-  homeButton.addEventListener("click", () => {
-    navigateTo("/Home", props);
-  });
+  // homeButton.addEventListener("click", () => {
+  //   navigateTo("/Home", props);
+  // });
 
   const sendButton = document.createElement("button");
   viewIndividualChat.appendChild(sendButton);
   sendButton.id = "send-button";
+  sendButton.className = "send-button";
   const sendText = document.createTextNode("send");
   sendButton.appendChild(sendText);
 
