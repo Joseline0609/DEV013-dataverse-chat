e583e22a4c49e35632d7b0e15083dd2dd7249abe
Joseline0609 <jkgarciaalfaro@gmail.com>
2024-02-28 21:38:50 -0600

    centering elements in welcome view

--- src/views/Group.js
+++ src/views/Group.js
@@ -1,34 +1,62 @@
-import {HomeIconButton} from "../components/HomeIconButton.js";
+//import {HomeIconButton} from "../components/HomeIconButton.js";
 
 export const GroupChat = () =>  {
   const viewGroupChat = document.createElement("section");
-  const main = document.createElement("main");
+  viewGroupChat.className="group-chat";
 
   viewGroupChat.innerHTML = `<div class="group-view">
+
   <section id="chat-area" class="chat-area">
     <div class="title">
-  <h1 id="title">Chat Grupal</h1>
+      <h1 id="title">Chat Grupal</h1>
+    </div>
+    <div class="message-area">
+    <div class="messages">
+      <div class="first-user-message">
+        <p>Hola, cómo están? Pueden decirme en que mes florecen?</p>
+      </div>
+      <div class="plant-answer">
+        <img alt="" src="" />
+        <p>Hola, yo </p>
+      </div>
+      <div class="plant-answer">
+        <img alt="" src="" />
+        <p>Hola, yo </p>
+      </div>
+      <div class="plant-answer">
+        <img alt="" src="" />
+        <p>Hola, yo </p>
+      </div>
+      </div>
+      <div class="textarea-group">
+      <textarea placeholder="Escribe tu mensaje aca" id="textarea-group" 
+      class="textarea-group-inside" required>
+      </textarea>
+      <button><img alt="" src=""/></button>
+      </div>
+    </div>
+</section>
+<div class="other">
+<section id="questions-ideas">
+  <div class="questions-title">
+    <h2>Puedes iniciar tu chat preguntando </br>
+      acerca de estas temáticas</h2>
   </div>
-    <div class="message-area">Zona de Mensajeria</div>
-  </section>
-
-  <section id="carrusel">
-
-    <h4>Puedes iniciar tu chat con estas tematicas</h4>
+  <div class="question-topics">
     <ul>
-      <li>Familia Botanica</li>
+      <li>Familia Botánica</li>
       <li>Cuidados</li>
-      <li>Floracion</li>
+      <li>Floración</li>
       <li>Mantenimiento</li>
       <li>Usos</li>
     </ul>
-  </section>
-  <div class="group-background">
-    <img src="image.png" alt="fondo-chat-grupal"/>
   </div>
-  </div>
+</section>
+<div class="group-background">
+  <img src="../Resources/DV Chat/group-background.png" alt="fondo-chat-grupal" />
+</div>
+</div>
+</div>
   `;
-
-  viewGroupChat.append(main, HomeIconButton());
   return viewGroupChat;
 }
