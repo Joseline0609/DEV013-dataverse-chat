e05540ef81fe65b7001e847221416f44a09300a7
Etelbina <etelbinacanedo@gmail.com>
2024-03-01 09:07:41 -0600

    temporal

--- src/components/EmptyApiKey.js
+++ src/components/EmptyApiKey.js
@@ -4,21 +4,20 @@ export default function EmptyApiKey() {
   const viewEmptyModal = document.createElement("dialog");
   viewEmptyModal.className = "empty-key modal";
   viewEmptyModal.innerHTML += `
-  <form id="epmty-apikey">
-  <form method="dialog">
-    <p class="message">
-      No has ingresado una llave <br />
-      para esta zona del jardín
-    </p>
-    <input type="text" required placeholder="Ingresa una Api Key..." /><br />
-    <button id="send-key-button1">Enviar</button>
-    <p class="link">
-    Si no tienes una llave solicítala <br />
-    haciendo click <a href="https://www.maisieai.com/help/how-to-get-an-openai-api-key-for-chatgpt" target="_blank">aquí</a>
-  </p>
-  <img id="close-icon1" class="close-icon" alt="Cerrar" src="Resources/DV Chat/Close.png">
-  </form>
-    `;
+    <form id="epmty-apikey" method="dialog">
+      <p class="message">
+        No has ingresado una llave <br />
+        para esta zona del jardín
+      </p>
+      <input type="text" required placeholder="Ingresa una Api Key..." /><br />
+      <button id="send-key-button1">Enviar</button>
+      <p class="link">
+        Si no tienes una llave solicítala <br />
+        haciendo click <a href="https://www.maisieai.com/help/how-to-get-an-openai-api-key-for-chatgpt" target="_blank">aquí</a>
+      </p>
+      <img id="close-icon1" class="close-icon1" alt="Cerrar" src="Resources/DV Chat/Close.png">
+    </form>
+  `;
 
   const sendKeyButon1 = viewEmptyModal
     .querySelector("#send-key-button1")
--- src/components/NotValidApiKey.js
+++ src/components/NotValidApiKey.js
@@ -4,21 +4,20 @@ export default function NotValidApiKey() {
   const viewValidModal = document.createElement("dialog");
   viewValidModal.className = "valid-key modal";
   viewValidModal.innerHTML += `
-  <form id="not-valid">
     <form id="not-valid" method="dialog">
       <p>
-      Tu llave no abre esta zona del jardín. <br/>
-      Ingresa una llave válida
+        Tu llave no abre esta zona del jardín. <br/>
+        Ingresa una llave válida
       </p>
       <input type="text" required placeholder="Ingresa Api Key válida..." /><br />
       <button id="send-key-button2">Enviar</button>
       <p>
-      Si no tienes una llave solicítala <br />
-      haciendo click <a href="https://www.maisieai.com/help/how-to-get-an-openai-api-key-for-chatgpt" target="_blank">aquí</a>
-    </p>
+        Si no tienes una llave solicítala <br />
+        haciendo click <a href="https://www.maisieai.com/help/how-to-get-an-openai-api-key-for-chatgpt" target="_blank">aquí</a>
+      </p>
       <img id="close-icon2" alt="Cerrar" src="Resources/DV Chat/closered.png">
     </form>
-      `;
+  `;
 
   const sendKeyButon2 = viewValidModal
     .querySelector("#send-key-button2")
